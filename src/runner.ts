1147dfe84257124beaf8c82056b575e832ce5196
Maarten Duhoux <maarten.duhoux@era.europa.eu>
2025-01-22 21:39:09 +0100

    integrated as a package

--- src/runner.ts
+++ src/runner.ts
@@ -1,19 +1,21 @@
-import { VPWParser } from './vpw-file-parser';
+import { VPWParser } from 'vpw-core';
 import { langGetal } from './lang-getal';
 
 async function main() {
-    const parser = new VPWParser<number>(
+    const parser = new VPWParser<number, number>(
         'in.txt',
         'uit.txt',
         (value: string) => Number(value),
         (line: string) => {
             const [index, value] = line.split(' ').map(Number);
             return [index, value];
-        }
+        },
+        undefined, // default compareOutputs
+        false  // first line is not a test count
     );
     
     await parser.initialize();
-    parser.runner(langGetal);
+    parser.simpleRunner(langGetal);
 
     const errors = parser.errorInputs;
     const testCount = parser.testsExecuted;
